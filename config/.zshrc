a17f69f422b3230ab048e0693751eada48ba3e0f
Adam James-Liebau Johnson <riizade@gmail.com>
2018-10-23 19:34:00 -0700

    fixed username

--- config/.zshrc
+++ config/.zshrc
@@ -4,7 +4,7 @@ fpath+=~/.zfunc
 export PATH=$HOME/bin:/usr/local/bin:$HOME/.poetry/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH="/home/riizade/.oh-my-zsh"
+  export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
-sudo apt-get install code
+sudo apt-get install code -y
 
 # install Google Chrome
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'

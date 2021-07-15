8c16070cb7147d5d757ac7fe8b0066fc6c5f230c
Adam James-Liebau Johnson <riizade@gmail.com>
2021-07-14 17:12:29 -0700

    switch to fish and starship

--- config/.zshrc
+++ config/.zshrc
@@ -8,12 +8,12 @@ export VIRTUALENVWRAPPER_PYTHON="/usr/bin/python3"
 export PATH=$HOME/bin:/usr/local/bin:/usr/bin/python3:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH="$HOME/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="powerlevel9k/powerlevel9k"
+# ZSH_THEME="powerlevel9k/powerlevel9k"
 
 # Set list of themes to load
 # Setting this variable when ZSH_THEME=random
@@ -67,13 +67,13 @@ ZSH_THEME="powerlevel9k/powerlevel9k"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git virtualenv virtualenvwrapper)
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time virtualenv)
+# plugins=(git virtualenv virtualenvwrapper)
+# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
+# POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time virtualenv)
 
 
-source $ZSH/oh-my-zsh.sh
-source /usr/local/bin/virtualenvwrapper.sh
+# source $ZSH/oh-my-zsh.sh
+# source /usr/local/bin/virtualenvwrapper.sh
 
 # User configuration
 
@@ -103,3 +103,6 @@ source /usr/local/bin/virtualenvwrapper.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# enable starship prompt
+eval "$(starship init zsh)"
--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -3,6 +3,9 @@ sudo apt-get install git -y
 sudo apt-get install curl -y
 sudo apt-get install zsh -y
 
+# install Starship
+sudo snap install starship
+
 # install python stuff
 sudo apt-get install python3-pip python3 build-essential -y
 sudo pip3 install --upgrade pip3
@@ -13,25 +16,25 @@ sudo pip3 install virtualenvwrapper
 git config --global user.email "riizade@gmail.com"
 git config --global user.name "Adam James-Liebau Johnson"
 
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
-# install powerlevel9k theme
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-# configure zsh
-mkdir ~/.zfunc
-cp ./config/.zshrc ~/.zshrc
-# install powerline fonts
-git clone https://github.com/powerline/fonts.git ~/Downloads/fonts
-cp -a ~/Downloads/fonts/SourceCodePro/. ~/.local/share/fonts/
+# install fish
+sudo apt-add-repository ppa:fish-shell/release-3 -y
+sudo apt-get update
+sudo apt-get install fish -y
+# configure fish
+cp ../config/config.fish ~/.config/fish/config.fish
+
+# install fonts
+cp -a ./fonts/. ~/.local/share/fonts
 fc-cache -vf ~/.local/share/fonts
+
 # configure terminal settings
-dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ./config/riizade.dconf
+dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ../config/riizade.dconf
 
 # install tmux and plugins
 sudo apt-get install tmux -y
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp ./config/.tmux.conf ~/.tmux.conf
+cp ../config/.tmux.conf ~/.tmux.conf
 
 # install Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -44,8 +47,8 @@ sudo apt-get install code -y
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 curl https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add
 sudo apt-get update
-sudo apt-get install google-chrome-stable
-sudo apt-get install chrome-gnome-shell
+sudo apt-get install google-chrome-stable -y
+sudo apt-get install chrome-gnome-shell -y
 
 # install visual modifications
-sudo apt-get install gnome-tweaks
+sudo apt-get install gnome-tweaks -y
